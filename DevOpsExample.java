f9e570a5d370185c65b57442c5064e2de5999dd1
Vidya-S60 <2020wb86151@wilp.bits-pilani.ac.in>
2024-06-07 17:34:01 +0000

    Update DevOpsExample.java

--- DevOpsExample.java
+++ DevOpsExample.java
@@ -1,5 +1,5 @@
-public class DevOpsExample{
-  public static void main(String[] args){
-    System.out.println(" Welcome to Devops. ");
+public class DevOpsExample {
+  public static void main(String[] args) {
+    System.out.println("Welcome to Devops.");
   }
 }
